950cd9432d15e4e0f87da5a0200ca09832444704
Spillz <billygcasey@hotmail.com>
2018-03-29 09:18:31 -0500

    got all feild verifications working

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ formU = """
                     <td><label for="email">Email (optional)</label></td>
                     <td>
                         <input name="email" value="">
-                        <span class="error"></span>
+                        <span class="error">{emailfail}</span>
                     </td>
                 </tr>
             </table>
@@ -61,41 +61,79 @@ def index():
     fill_in_content = formU
     return fill_in_content
 
-@app.route("/", methods=['POST'])
-def UID_validate():  #Username verification
-    usernameA = request.form['username']
-    uidX = 0 #tests uid length p1
-    for char in usernameA:
-        uidX =  uidX + 1
-    
-    if uidX < 3 or uidX > 20:
-        uidlngth = "User ID must be 3 - 20 characters."
-        return formU.format(usernamefail=uidlngth)
-
 
 
 
 @app.route("/", methods=['POST'])
-def password_validate():  #Password verification
+def password_validate():  #Actually all validate
+    #Password Validation
     passwordA = request.form['password']
     verifyA = request.form['verify']
+    usernameA = request.form['username']
+    emailA = request.form['email']
+
     invalid_char = ''   #tests for " " in pw p1
     pwX = 0             #tests pw length p1
+    uidX = 0            #tests uid length p1
+    emlX = 0             #tests email length
+
+    pswrdfailA = ''
+    usernamefailA = ''
+    emailfailA = ''
+    at_test = ''
+    dot_test = ''
+    #pwd
     for char in passwordA:
         pwX = pwX + 1
-        if char == " ":   #tests for " " in pw p2
-            invalid_char = "Invalid Character. Cannot contain 'space'"
-    if invalid_char == "Invalid Character. Cannot contain 'space'":      
-        return formU.format(pswrdfail=invalid_char)
-    if passwordA == '':     #Test for blank pw
-        pwsblank = "Password can not be blank, dumbass."
-        return formU.format(pswrdfail=pwsblank)
-    if pwX < 3 or pwx > 20:       #tests pw length p2
-        pwlngth = "Password must be 3 - 20 characters."
-        return formU.format(pswrdfail=pwlngth)           
-    if passwordA != verifyA: #Tests to see if pws match
-        nomatch = "Passwords do not match, idiot."
-        return formU.format(pswrdfail=nomatch)
+        if char == " ":   #tests for " " in udi
+            pswrdfailA = "Invalid Character. Cannot contain 'space'"
+    if passwordA == '':     #Test for blank uid
+         pswrdfailA = "Password can not be blank, dumbass."
+    elif pwX < 3 or pwX > 20:       #tests pw length 
+        pswrdfailA = "Password must be 3 - 20 characters."
+    elif passwordA != verifyA: #Tests to see if pws match
+        pswrdfailA = "Passwords do not match, idiot."
+    #UID
+    for char in usernameA:
+        uidX =  uidX + 1
+        if char == " ":   #tests for " " in UID
+            usernamefailA = "User ID cannot contain 'space'"
+    if uidX < 3 or uidX > 20:
+        usernamefailA = "User ID must be 3 - 20 characters."
+    if usernameA == '':     #Test for blank UID
+         usernamefailA = "You must enter a user ID."
+    #email
+    for char in emailA:
+        emlX = emlX + 1
+        if char == " ":   #tests for " " in emial
+            emailfailA = "Email addresses dont have spaces, jerk."
+        if char == "@":    #tests for @ and .
+            at_test = emlX
+        if char == ".":
+            dot_test = emlX
+    if emlX < 3 or emlX > 20:   #tests length.
+        emailfailA = "Email must be 3 - 20 characters, for some reason." 
+    if at_test and dot_test:   #tests for @ and . in correct order
+        if at_test < dot_test:
+            testvaraible_unused = "pass"
+        else:
+            emailfailA = "Invalid email."
+    else:
+        emailfailA = "Invalid email." 
+    if emlX == 0:    # ignores count if email is blank
+            emailfailA = ''
+
+
+
+
+
+    if pswrdfailA or usernamefailA or emailfailA:      
+        return formU.format(pswrdfail=pswrdfailA, usernamefail=usernamefailA, emailfail=emailfailA)
+    else: 
+        return '<h1>Hello, ' + usernameA + ", and welcome to our nacho barn!</h1>"
+
+
+    
 
 
 
